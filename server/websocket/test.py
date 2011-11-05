e58b6ca6b6d775cfc281b9cecb2e3eafa2f8f291
Kristoffer Kleine <kris.kleine@yahoo.de>
2011-11-05 21:47:18 +0100

    Progress

--- server/websocket/test.py
+++ server/websocket/test.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
+import sys
+import time
+import signal
 import base64
 import socket
 import hashlib
-import threading
-
-import time
-import struct
+from select import select
+from threading import Thread
 
 
 HANDSHAKE = '''HTTP/1.1 101 Switching Protocols\r
 Upgrade: websocket\r
 Connection: Upgrade\r
-Sec-WebSocket-Accept: {key}
-\r\n'''
+Sec-WebSocket-Accept: {key}\r\n\r\n'''
 
 
 MAGIC_KEY = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
@@ -34,46 +34,36 @@ def split_handshake_request(request):
 
 class WebSocket(object):
 
-    def __init__(self, host, port):
+    def __init__(self, client, host, port):
 
+        self.client = client
         self.host = host
         self.port = port
 
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((host, port))
-        self.socket.listen(10)
-
         self.handshaken = False
-
         self.data = ''
 
-        self.client, adress = self.socket.accept()
 
-        self.handle()
+    def feed(self, data):
+
+        if not self.handshaken:
+            self.do_handshake(data)
+        else:
+            self.data += data
+            print self.data.decode('utf-8', 'ignore')
 
 
-    def handle(self):
+    def onmessage(self, msg):
 
-        while True:
-            if not self.handshaken:
-                data = self.client.recv(255)
-                data = data.decode('utf-8', 'ignore')
-                self.do_handshake(data)
-            else:
-                msg = self.client.recv(4096)
-                if msg.startswith('\x88\x80'): # Close frame
-                    self.close(msg)
-                else:
-                    self.echo(msg)
-
+        print 'Got message: {0}'.format(msg)
 
 
     def do_handshake(self, request):
 
         request = split_handshake_request(request)
+
         key = generate_accept_token(request['Sec-WebSocket-Key'])
         origin = request['Sec-WebSocket-Origin'].strip()
-
         handshake = HANDSHAKE.format(key=key, origin=origin, host=self.host, port=self.port)
 
         self.handshaken = True
@@ -81,22 +71,63 @@ class WebSocket(object):
         self.client.send(handshake)
 
 
-    def close(self, msg):
 
-        frame = struct.pack('B', 0x80 | 0x8)
-        frame += struct.pack('B', 0)
+class WebSocketServer(object):
 
-        self.client.send(frame)
+    def __init__(self, host, port):
 
-        self.socket.close()
+        self.host = host
+        self.port = port
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((host, port))
+
+        self.connections = {}
+        self.listeners = [self.socket]
+        self.running = True
+
+    def listen(self):
+
+        self.socket.listen(5)
+        while self.running:
+            rlist, wlist, xlist = select(self.listeners, [], self.listeners, 1)
+            for ready in rlist:
+                if ready == self.socket:
+                    client, address = self.socket.accept()
+                    fileno = client.fileno()
+                    self.listeners.append(fileno)
+                    self.connections[fileno] = WebSocket(client, self.host, self.port)
+                else:
+                    client = self.connections[ready].client
+                    data = client.recv(1024)
+                    fileno = client.fileno()
+                    if data:
+                        self.connections[fileno].feed(data)
+                    else:
+                        pass
+
+            for failed in xlist:
+                if failed == self.socket:
+                    print 'Socket broke'
+                    for fileno, conn in self.connections:
+                        conn.close()
+                    self.running = False
 
 
-
-    def echo(self,  msg):
-
-        self.client.send('\x00' + 'Test'.encode('utf-8') + '\xff')
 
 
 
 if __name__ == '__main__':
-    sock = WebSocket('127.0.0.1', 8086)
+
+    server = WebSocketServer('localhost', 8080)
+    server_thread = Thread(target=server.listen)
+    server_thread.start()
+
+    def signal_handler(signal, frame):
+        print 'Exiting...'
+        server.running = False
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
+    while True:
+        time.sleep(100)
